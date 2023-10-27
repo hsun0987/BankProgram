9506f0d2d4b9749cd279878b0269c8bd23c3b9b4
mshan <hsun0987@daum.net>
2023-10-27 16:19:47 +0900

    update

--- src/main/java/bank/AccountEx.java
+++ src/main/java/bank/AccountEx.java
@@ -1,2 +1,91 @@
-package bank;public class AccountEx {
+package bank;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class AccountEx {
+    static ArrayList<Account> accounts = new ArrayList();
+    public static void main(String[] args) {
+        // 사용자로부터 입력
+        Scanner scanner = new Scanner(System.in);
+
+        int end = 0; // while문 끝
+
+        while (true){
+
+            if (end == 1) break;
+
+            System.out.println("---------------------------------------------");
+            System.out.println("1.계좌생성 | 2.계좌목록 | 3.예금 | 4.출금 | 5.종료");
+            System.out.println("---------------------------------------------");
+            System.out.print("선택 > ");
+
+            int op = Integer.parseInt(scanner.nextLine());
+            switch (op){
+                case 1:
+                    System.out.println("---------");
+                    System.out.println(" 계좌생성 ");
+                    System.out.println("---------");
+
+
+                    System.out.print("계좌번호: ");
+                    String aId = scanner.nextLine();
+
+                    System.out.print("계좌주: ");
+                    String aName = scanner.nextLine();
+
+                    System.out.print("초기입금액: ");
+                    int aMon = Integer.parseInt(scanner.nextLine());
+
+                    accounts.add(new Account(aName, aId, aMon));
+                    System.out.println("결과: 계좌가 생성되었습니다.");
+                    break;
+
+                case 2:
+                    System.out.println("---------");
+                    System.out.println(" 계좌목록 ");
+                    System.out.println("---------");
+
+                    for (Account account : accounts){
+                        System.out.println(account.id + " " + account.name + " " + account.money);
+
+                    }
+                    break;
+                case 3:
+                    System.out.println("---------");
+                    System.out.println(" 예금 ");
+                    System.out.println("---------");
+                    System.out.print("계좌번호: ");
+                    aId = scanner.nextLine();
+                    System.out.print("예금액: ");
+                    int dep = Integer.parseInt(scanner.nextLine());
+                    for (Account account : accounts){
+                        if (account.id.equals(aId))
+                            account.money += dep;
+
+                    }
+                    break;
+                case 4:
+                    System.out.println("---------");
+                    System.out.println(" 출금 ");
+                    System.out.println("---------");
+                    System.out.print("계좌번호: ");
+                    aId = scanner.nextLine();
+                    System.out.print("출금액: ");
+                    int with = Integer.parseInt(scanner.nextLine());
+
+                    for (Account account : accounts){
+                        if (account.id.equals(aId))
+                            account.money -= with;
+                    }
+                    System.out.println("결과: 출금이 성공되었습니다.");
+                    break;
+                case 5:
+                    System.out.println("프로그램 종료");
+                    end = 1;
+                    break;
+            }
+        }
+
+    }
 }
--- src/main/java/bank/BankEx.java
+++ src/main/java/bank/BankEx.java
@@ -1,2 +1,98 @@
-package bank;public class BankEx {
+package bank;
+
+import java.util.Scanner;
+
+// 정적 배열 ver
+
+public class BankEx {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        Account[] acList = new Account[100];
+
+        int i = 0;
+        int end = 0;
+
+
+        while (true){
+
+            if (end == 1) break;
+
+            System.out.println("---------------------------------------------");
+            System.out.println("1.계좌생성 | 2.계좌목록 | 3.예금 | 4.출금 | 5.종료");
+            System.out.println("---------------------------------------------");
+            System.out.print("선택 > ");
+
+            int op = Integer.parseInt(scanner.nextLine());
+            switch (op){
+                case 1:
+                    System.out.println("---------");
+                    System.out.println(" 계좌생성 ");
+                    System.out.println("---------");
+
+
+                    System.out.print("계좌번호: ");
+                    String aNum = scanner.nextLine();
+
+                    System.out.print("계좌주: ");
+                    String aName = scanner.nextLine();
+
+                    System.out.print("초기입금액: ");
+                    int aMon = Integer.parseInt(scanner.nextLine());
+
+                    acList[i] = new Account(aName, aNum, aMon);
+                    System.out.println("결과: 계좌가 생성되었습니다.");
+                    i++;
+                    break;
+                case 2:
+                    System.out.println("---------");
+                    System.out.println(" 계좌목록 ");
+                    System.out.println("---------");
+
+                    for (int j = 0; j < acList.length; j++){
+                        if (acList[j] != null) {
+                            System.out.println(acList[j].id + "  " + acList[j].name + "  " + acList[j].money);
+                        }
+                    }
+                    break;
+                case 3:
+                    System.out.println("---------");
+                    System.out.println(" 예금 ");
+                    System.out.println("---------");
+                    System.out.print("계좌번호: ");
+                    aNum = scanner.nextLine();
+                    System.out.print("예금액: ");
+                    int dep = Integer.parseInt(scanner.nextLine());
+                    for (int j=0; j < acList.length;j++){
+                        if (acList[j] != null) {
+                            if (acList[j].id.equals(aNum))
+                                acList[j].money += dep;
+                        }
+                    }
+                    break;
+                case 4:
+                    System.out.println("---------");
+                    System.out.println(" 출금 ");
+                    System.out.println("---------");
+                    System.out.print("계좌번호: ");
+                    aNum = scanner.nextLine();
+                    System.out.print("출금액: ");
+                    int with = Integer.parseInt(scanner.nextLine());
+
+                    for (int j=0; j < acList.length;j++){
+                        if (acList[j] != null) {
+                            if (acList[j].id.equals(aNum))
+                                acList[j].money -= with;
+                        }
+                    }
+                    System.out.println("결과: 출금이 성공되었습니다.");
+                    break;
+                case 5:
+                    System.out.println("프로그램 종료");
+                    end = 1;
+                    break;
+            }
+        }
+
+    }
 }
